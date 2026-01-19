751dd2b6ab0093ce816ae374e9b6a0409c441368
Steve Wilshire <34696631+cmptrblder@users.noreply.github.com>
2026-01-19 08:30:08 -0500

    Delete smart_oil_gauge/custom_components/smart_oil_gauge/custom_components/smart_oil_gauge directory

--- smart_oil_gauge/custom_components/smart_oil_gauge/custom_components/smart_oil_gauge/const.py
+++
@@ -1,7 +0,0 @@
-DOMAIN = "smart_oil_gauge"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-
-BASE_URL = "https://app.smartoilgauge.com"
-AJAX_URL = "https://app.smartoilgauge.com/ajax/main_ajax.php"
-LOGIN_URL = "https://app.smartoilgauge.com/login.php"
